fb735bfd362f04cbbf1f588020223ba2b7940f44
Arthur Granado <agranado2k@gmail.com>
2019-03-23 22:50:44 +0000

    Solve Rubocop offenses

--- spec/features/query_books_spec.rb
+++ spec/features/query_books_spec.rb
@@ -1,19 +1,21 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.feature "QueryBooks", type: :feature do
   context "when visiting query books page" do
     it "should show search page empyt" do
-      visit '/books'
+      visit "/books"
 
-      expect(page).to have_content 'Books page'
-      expect(page).to have_field(id: 'search_field')
-      expect(page).to have_button 'Search'
+      expect(page).to have_content "Books page"
+      expect(page).to have_field(id: "search_field")
+      expect(page).to have_button "Search"
     end
 
     it "should default route to books page" do
-      visit '/'
+      visit "/"
 
-      expect(page).to have_current_path '/books'
+      expect(page).to have_current_path "/books"
     end
   end
 end
