401d2ed4f4a4684af5b08d23880581ff1f24c311
Arthur Granado <agranado2k@gmail.com>
2019-03-24 10:54:48 +0000

    Create Use Case to query a Book, tests are failing so far

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -12,7 +12,24 @@ RSpec.describe BooksController, type: :controller do
       end
 
       it 'books is assigned to empty list' do
-        expect(assigns(:books)).to eq([])
+        expect(assigns(:books)).to be_empty
+      end
+
+      it "renders the index template" do
+        expect(response).to render_template("index")
+      end
+    end
+
+    # TODO: Share test instead duplicate it
+    context 'when the search query is NOT empty' do
+      before { get :index, params: {q: "sail"} }
+
+      it 'returns HTTP status 200' do
+        expect(response).to have_http_status :ok
+      end
+
+      it 'books is assigned to empty list' do
+        expect(assigns(:books)).to_not be_empty
       end
 
       it "renders the index template" do
--- spec/features/query_books_spec.rb
+++ spec/features/query_books_spec.rb
@@ -31,7 +31,18 @@ RSpec.feature "QueryBooks", type: :feature do
         end
       end
 
-      context "when query is not empty" do
+      context "when fill query with some word" do
+        before(:each) do
+          fill_in 'search_field', with: 'sail'
+          click_button 'Search'
+        end
+
+        it "should return query books page without any results" do
+          expect(page).to_not have_content('No results')
+          expect(page).to have_content('Title')
+          expect(page).to have_content('Author')
+          expect(page).to have_content('Publisher')
+        end
 
       end
     end
