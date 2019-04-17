a0c6b90d98c75d6e2c37e841014fa3c009f33e17
Arthur Granado <agranado2k@gmail.com>
2019-04-17 21:45:24 +0100

    Change page in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,29 @@
 
 class BooksController < ApplicationController
   def index
+    @current_page = param_current_page
     @books = []
     return if params[:search_field].blank?
 
     google_api_key = Rails.application.credentials.google_api_key!
     book_api = GoogleBookApiService.new(google_api_key)
     book_repository = BookRepository.new(book_api)
-    @books = book_repository.search(params[:search_field], current_page)
+    @books = book_repository.search(params[:search_field], @current_page)
   end
 
   private
 
-    def current_page
-      return 1 if params[:currentPage].blank?
+    def param_current_page
+      page = params[:current_page].nil? ? 0 :  params[:current_page].to_i
+      action = params[:page_action]
+      if !action.nil? && action == 'forward'
+        page += 1
+      end
 
-      params[:currentPage]
+      if !action.nil? && action == 'backward' && page > 0
+        page -= 1
+      end
+
+      page
     end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -30,9 +30,24 @@ RSpec.describe BooksController, type: :controller do
           expect(response).to render_template("index")
         end
       end
-      let(:params) { { search_field: "sail", currentPage: 1 } }
+      let(:params) { { search_field: "sail", current_page: 0 } }
 
-      context "when currentPage has defualt value" do
+      context "when current_page has defualt value and no page_action" do
+        before { get :index, params: params }
+
+        include_context "successful request"
+
+        it "books is assigned to empty list" do
+          expect(assigns(:books)).to_not be_empty
+        end
+
+        it "current_page keep same" do
+          expect(assigns(:current_page)).to eq(0)
+        end
+      end
+
+      context "when has page_action foward" do
+        let(:params) { { search_field: "sail", current_page: 0 } }
         before { get :index, params: params }
 
         include_context "successful request"
@@ -42,24 +57,58 @@ RSpec.describe BooksController, type: :controller do
         end
       end
 
-      context "when currentPage has defualt value + 1" do
-        before { get :index, params: params }
-
-        include_context "successful request"
-
-        it "books is assigned to empty list" do
-          expect(assigns(:books)).to_not be_empty
-        end
-      end
-
-      context "when currentPage has defualt value + 1000" do
-        let(:params) { { search_field: "sail", currentPage: 1000 } }
+      context "when current_page has defualt value + 1000" do
+        let(:params) { { search_field: "sail", current_page: 1000 } }
         before { get :index, params: params }
 
         include_context "successful request"
 
         it "books is assigned to empty list" do
           expect(assigns(:books)).to be_empty
+        end
+      end
+
+      context "when change current_page" do
+        context "when has page_action forward" do
+          let(:params) do
+            {
+              search_field: "sail",
+              current_page: 0,
+              page_action: "forward"
+            }
+          end
+          before { get :index, params: params }
+          it "should inc current_page by 1" do
+            expect(assigns(:current_page)).to eq(1)
+          end
+        end
+
+        context "when has page_action backward" do
+          let(:params) do
+            {
+              search_field: "sail",
+              current_page: 10,
+              page_action: "backward"
+            }
+          end
+          before { get :index, params: params }
+          it "should dec current_page by 1" do
+            expect(assigns(:current_page)).to eq(9)
+          end
+
+          context "when current_page is already 0" do
+            let(:params) do
+              {
+                search_field: "sail",
+                current_page: 0,
+                page_action: "backward"
+              }
+            end
+            before { get :index, params: params }
+            it "should not dec current_page by 1" do
+              expect(assigns(:current_page)).to eq(0)
+            end
+          end
         end
       end
     end
